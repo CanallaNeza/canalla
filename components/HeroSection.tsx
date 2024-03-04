52ca0aad4dfc04671fc0f9fff9ef43c5a708d82c
crbzxy <carlos.boor@gmail.com>
2024-03-03 18:57:41 -0600

    update and refactor

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -11,31 +11,28 @@ const HeroSection = () => {
       <section
         id='home'
         className='flex flex-col items-center justify-center my-10 py-16 text-center animate-fadeIn animation-delay-2 sm:py-32 md:flex-row md:space-x-4  md:text-left'>
-        <div className='flex-shrink-0 md:w-1/2 lg:flex lg:justify-center'>
+        <div className='flex-shrink-0 md:w-1/2 lg:flex lg:justify-center ' >
           <Image
             src='/head.gif' // Reemplaza "/miImagen.png" con la ruta a tu imagen de perfil
             alt='Carlos Armando Boyzo Oregón, Diseñador UX y Desarrollador Front End'
             width={325}
             height={325}
-            className='rounded-full shadow-2xl'
+            className='rounded-full shadow-2xl bg-black'
             priority // Esto asegura que la imagen se cargue con prioridad
           />
         </div>
         <div className='md:w-3/5'>
-          <h1 className='mt-6 text-4xl font-bold md:mt-0 md:text-4xl'>
-            ¡Hola, soy Carlos Armando Boyzo Oregón!
-          </h1>
-          <p className='mt-4 mb-6 text-lg md:text-2xl'>
-            Soy un{' '}
-            <span className='font-semibold text-teal-600'>
-              Diseñador UX y Desarrollador Front End
-            </span>{' '}
-            con una pasión por integrar el arte en la tecnología, creando
-            experiencias digitales únicas que mejoran la vida de las personas.
-          </p>
+  <h1 className='mt-6 text-4xl font-bold md:mt-0 md:text-4xl'>
+    ¡Hola, soy Carlos Armando Boyzo Oregón!
+  </h1>
+  <p className='mt-4 mb-6 text-lg md:text-2xl'>
+    Como <span className='font-semibold text-indigo-500'>Desarrollador UX</span>, creo experiencias digitales que unen arte y tecnología, impulsando los objetivos comerciales de mis clientes. Mi enfoque está en soluciones innovadoras que mejoren la vida de las personas.
+  </p>
+
+
           <Link
             to='projects'
-            className='inline-block px-6 py-3 text-neutral-100 bg-teal-600 rounded shadow font-semibold hover:bg-teal-700 transition-colors duration-300 ease-in-out'
+            className='inline-block px-6 py-3 text-neutral-100 bg-gray-800 rounded shadow font-semibold hover:bg-teal-700 transition-colors duration-300 ease-in-out'
             activeClass='active'
             spy={true}
             smooth={true}
