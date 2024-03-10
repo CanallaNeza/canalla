89cfd87e5e5b1265f465ceefce53d2c6811393e0
crbzxy <carlos.boor@gmail.com>
2024-03-10 17:49:30 -0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-"use client"
-import "../styles/globals.css"
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import { ThemeProvider } from "next-themes"
+'use client';
+import '../styles/globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { ThemeProvider } from 'next-themes';
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en">
+    <html lang='es-mx'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="dark:bg-stone-900">
-        <ThemeProvider enableSystem={true} attribute="class">
+      <body className='dark:bg-gray-950'>
+        <ThemeProvider enableSystem={true} attribute='class'>
           <Navbar />
           {children}
           <Footer />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,40 +6,48 @@ import { HiArrowDown } from 'react-icons/hi';
 
 const HeroSection = () => {
   return (
-    <>
-      <section
-        id='home'
-        className='flex flex-col items-center justify-center my-10 py-16 text-center animate-fadeIn animation-delay-2 sm:py-32 md:flex-row md:space-x-4 md:text-left'>
-        <div className='flex-shrink-0 md:w-1/2 lg:flex lg:justify-center'>
-          <Image
-            src='/head.gif'
-            alt='Carlos Armando Boyzo Oregón, Diseñador UX y Desarrollador Front End'
-            width={325}
-            height={325}
-            className='rounded-full shadow-2xl bg-black'
-            priority
-          />
-        </div>
-        <div className='md:w-3/5'>
-          <h1 className='mt-6 text-4xl font-bold md:mt-0 md:text-4xl'>
-            ¡Hola, soy <br /><span className='font-semibold text-indigo-500'>Carlos Boyzo Oregon</span>!
-          </h1>
-          <p className='mt-4 mb-6 text-lg md:text-xl'>
-            Como <span className='font-semibold text-indigo-500'>Desarrollador UX</span>, me enfoco en crear soluciones y experiencias digitales positivas, que conecten los objetivos comerciales de mis clientes en diferentes verticales de negocio.
-          </p>
-          <Link
-            to='projects'
-            className='inline-block px-6 py-3 text-neutral-100 bg-gray-800 rounded shadow font-semibold hover:bg-teal-700 transition-colors duration-300 ease-in-out'
-            activeClass='active'
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}>
-            Conoce más
-          </Link>
-        </div>
-      </section>
-      <div className='my-10 py-16 flex flex-row items-center text-center justify-center'>
+    <section
+      id='home'
+      className='flex flex-col items-center justify-center my-10 py-16 text-center animate-fadeIn animation-delay-2 sm:py-32 md:flex-row md:space-x-4 md:text-left'>
+      <div className='flex-shrink-0 md:w-1/2 lg:flex lg:justify-center'>
+        <Image
+          src='/head.gif'
+          alt='Carlos Armando Boyzo Oregón, Diseñador UX y Desarrollador Front End'
+          width={325}
+          height={325}
+          className='rounded-full shadow-2xl bg-black'
+          priority
+        />
+      </div>
+      <div className='md:w-1/2'>
+        <h1 className='mt-6 text-4xl font-bold md:mt-0 md:text-4xl'>
+          ¡Hola, soy <br />
+          <span className='font-semibold text-indigo-500'>
+            Carlos Boyzo Oregon
+          </span>
+          !
+        </h1>
+        <p className='mt-4 mb-6 text-lg md:text-xl'>
+          Como{' '}
+          <span className='font-semibold text-indigo-500'>
+            Desarrollador UX,{'\u00A0'}
+          </span>
+          me enfoco en crear soluciones y experiencias digitales positivas,que
+          conecten los objetivos comerciales de mis clientes en diferentes
+          verticales de negocio.
+        </p>
+        <Link
+          to='projects'
+          className='inline-block px-6 py-3 text-neutral-100 bg-gray-800 rounded shadow font-semibold hover:bg-teal-700 transition-colors duration-300 ease-in-out'
+          activeClass='active'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}>
+          Conoce más
+        </Link>
+      </div>
+      <div className='my-10 py-16 flex flex-row items-center text-center justify-center absolute bottom-0'>
         <Link
           to='about'
           activeClass='active'
@@ -50,7 +58,7 @@ const HeroSection = () => {
           <HiArrowDown size={35} className='animate-bounce' />
         </Link>
       </div>
-    </>
+    </section>
   );
 };
 
