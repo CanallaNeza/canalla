25f1d191cbd1b7011c344b691702273dc7c3c0d7
crbzxy <carlos.boor@gmail.com>
2024-03-17 15:40:45 -0600

    fix state

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
     <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
-        <div className="text-neutral-500 dark:text-neutral-100">
+        <div className="text-neutral-500 dark:text-neutral-500">
         <sup>©</sup> {currentYear} Canalla Neza
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
@@ -28,7 +28,7 @@ const Footer = () => {
               href={href}
               rel="noreferrer"
               target="_blank"
-              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-500"
             >
               <Icon size={30} />
             </a>
