10b25318e68354b2a3fbed1d7a0fcd2d117bd1f9
crbzxy <carlos.boor@gmail.com>
2024-03-17 17:18:34 -0600

    ajuste de meta

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,29 @@
 import Script from 'next/script';
+import Head from 'next/head'; // Asegúrate de importar Head desde 'next/head'
 
-export default function Head() {
+export default function CustomHead() {
   return (
     <>
-      <title>Canalla Neza</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Canalla Neza: Chácharas & arte De Cd Neza para el mundo!!☠️" />
-      <link rel="icon" href="../favicon.ico" />
+      <Head>
+        <title>Canalla Neza</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Canalla Neza: Chácharas & arte De Cd Neza para el mundo!!☠️" />
+        <link rel="icon" href="/favicon.ico" /> {/* Asegúrate de que el path esté correcto */}
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://canallaneza.art/" /> 
+        <meta property="og:title" content="Canalla Neza" />
+        <meta property="og:description" content="Canalla Neza: Chácharas & arte De Cd Neza para el mundo!!☠️" />
+        <meta property="og:image" content="https://canallaneza.art/canalla.jpg" /> 
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://canallaneza.art/" /> 
+        <meta property="twitter:title" content="Canalla Neza" />
+        <meta property="twitter:description" content="Canalla Neza: Chácharas & arte De Cd Neza para el mundo!!☠️" />
+        <meta property="twitter:image" content="https://canallaneza.art/canalla.jpg" /> 
+      </Head>
 
       {/* Google Tag Manager */}
       <Script
